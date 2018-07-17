bbba4d24c14f6819a0ae2b20fd27fecb5a7bb113
Yongzhou Xiang <yongzhoux@jauntvr.com>
2018-07-17 15:48:21 +0800

    first commit

--- src/com/company/playground.java
+++ src/com/company/playground.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -11,15 +8,73 @@ import java.util.List;
  */
 public class playground {
 
-    static class Solution {
-        public int lengthOfLongestSubstring(String s) {
-            return -1;
+    static class SolutionTemp {
+        public List<String> covertIpToCider(String ip, int length) {
+            List<String> ipSet = new ArrayList<>();
+            String[] ipInfo = ip.split(".");
+            int lastDigit = Integer.parseInt(ipInfo[3]);
+            int temp = lastDigit;
+            int counter = 0;
+            int factorCounter = 0;
+            if (temp == 0) {
+                while (counter + Math.pow(2, factorCounter) < length) {
+                    factorCounter++;
+                }
+                factorCounter--;
+                counter += Math.pow(2, factorCounter);
+                int digitInfo = 32 - factorCounter;
+                String outputString = ipInfo[0] + "." + ipInfo[1] + "." + ipInfo[2] + "." + lastDigit + "/" + digitInfo;
+                lastDigit += Math.pow(2, factorCounter);
+                temp = lastDigit;
+                ipSet.add(outputString);
+            }
+
+            for (; counter < length;) {
+                factorCounter = 0;
+                while (temp % 2 == 0) {
+                    temp = temp / 2;
+                    factorCounter++;
+                }
+                counter += Math.pow(2, factorCounter);
+                int digitInfo = 32 - factorCounter;
+                if (counter > length) {
+                    break;
+                }
+                String outputString = ipInfo[0] + "." + ipInfo[1] + "." + ipInfo[2] + "." + lastDigit + "/" + digitInfo;
+                lastDigit += Math.pow(2, factorCounter);
+                temp = lastDigit;
+                ipSet.add(outputString);
+            }
+            if (counter > length) {
+                counter = counter - (int) Math.pow(2, factorCounter);
+                while (counter < length) {
+                    if (counter + Math.pow(2, factorCounter) > length) {
+                        factorCounter--;
+                    } else {
+                        counter += Math.pow(2, factorCounter);
+                        int digitInfo = 32 - factorCounter;
+                        if (counter > length) {
+                            break;
+                        }
+                        String outputString = ipInfo[0] + "." + ipInfo[1] + "." + ipInfo[2] + "." + lastDigit + "/" + digitInfo;
+                        lastDigit += Math.pow(2, factorCounter);
+                        ipSet.add(outputString);
+                    }
+                }
+            }
+
+            return ipSet;
         }
     }
 
     public static void main(String[] args) {
         // write your code here
-        Solution solution = new Solution();
-
+        SolutionTemp solution = new SolutionTemp();
+        String ip = "255,12,0,0";
+        int length = 13;
+        List<String> outputs = solution.covertIpToCider(ip, length);
+        for (String output:outputs) {
+            System.out.println(output);
+        }
     }
 }
--- src/com/company/playground2.java
+++ src/com/company/playground2.java
@@ -1,7 +1,150 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * Created by xiangyongzhou on 18/7/13.
  */
 public class playground2 {
+    static HashSet<String> seenSituation = new HashSet<>();
+
+    static class Situation {
+        int[][] value = new int[3][3];
+        int blackI = -1;
+        int blackJ = -1;
+        public Situation(int[][] value) {
+            this.value = value;
+            for (int i=0; i < 3; i++) {
+                for (int j=0; j < 3; j++) {
+                    if (value[i][j] == -1) {
+                        blackI = i;
+                        blackJ = j;
+                        break;
+                    }
+                }
+            }
+        }
+
+        List<Situation> nextMove = new ArrayList<>();
+
+        public void initializeSituation() {
+            seenSituation.add(this.toString());
+            //System.out.println(seenSituation.size());
+
+            if (blackJ + 1 < 3) {
+                int[][] newV = new int[3][3];
+                for (int i=0; i < 3; i++) {
+                    for (int j=0; j < 3; j++) {
+                        newV[i][j] = value[i][j];
+                    }
+                }
+                newV[blackI][blackJ + 1] = -1;
+                newV[blackI][blackJ] = value[blackI][blackJ + 1];
+                Situation newS = new Situation(newV);
+                if (!seenSituation.contains(newS.toString())) {
+                    seenSituation.add(newS.toString());
+                    nextMove.add(newS);
+                }
+            }
+            if (blackI + 1 < 3) {
+                int[][] newV = new int[3][3];
+                for (int i=0; i < 3; i++) {
+                    for (int j=0; j < 3; j++) {
+                        newV[i][j] = value[i][j];
+                    }
+                }
+                newV[blackI + 1][blackJ] = -1;
+                newV[blackI][blackJ] = value[blackI + 1][blackJ];
+                Situation newS = new Situation(newV);
+                if (!seenSituation.contains(newS.toString())) {
+                    seenSituation.add(newS.toString());
+                    nextMove.add(newS);
+                }
+            }
+            if (blackI - 1 > -1) {
+                int[][] newV = new int[3][3];
+                for (int i=0; i < 3; i++) {
+                    for (int j=0; j < 3; j++) {
+                        newV[i][j] = value[i][j];
+                    }
+                }
+                newV[blackI - 1][blackJ] = -1;
+                newV[blackI][blackJ] = value[blackI - 1][blackJ];
+                Situation newS = new Situation(newV);
+                if (!seenSituation.contains(newS.toString())) {
+                    seenSituation.add(newS.toString());
+                    nextMove.add(newS);
+                }
+            }
+            if (blackJ - 1 > -1) {
+                int[][] newV = new int[3][3];
+                for (int i=0; i < 3; i++) {
+                    for (int j=0; j < 3; j++) {
+                        newV[i][j] = value[i][j];
+                    }
+                }
+                newV[blackI][blackJ - 1] = -1;
+                newV[blackI][blackJ] = value[blackI][blackJ - 1];
+                Situation newS = new Situation(newV);
+                if (!seenSituation.contains(newS.toString())) {
+                    seenSituation.add(newS.toString());
+                    nextMove.add(newS);
+                }
+            }
+        }
+
+        public boolean checkCorrect() {
+            initializeSituation();
+            boolean isCorrect = true;
+            for (int i=0; i < 3; i++) {
+                int base = 3*i;
+                for (int j=0; j < 3; j++) {
+                    if (value[i][j] == base + j + 1 || (i==2 && j==2 && value[i][j] == -1)) {
+
+                    } else {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+            }
+            if (isCorrect) {
+                return true;
+            } else {
+                for (Situation nextSituation : nextMove) {
+                    if(nextSituation.checkCorrect()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            for (int i=0; i < 3; i++) {
+                for (int j=0; j < 3; j++) {
+                    builder.append(value[i][j]);
+                }
+            }
+            return builder.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] initialSituation = new int[3][3];
+        initialSituation[0][0] = 1;
+        initialSituation[0][1] = 2;
+        initialSituation[0][2] = 3;
+        initialSituation[1][0] = 4;
+        initialSituation[1][1] = 5;
+        initialSituation[1][2] = 6;
+        initialSituation[2][0] = 7;
+        initialSituation[2][1] = -1;
+        initialSituation[2][2] = 8;
+        Situation initSituation = new Situation(initialSituation);
+        boolean result = initSituation.checkCorrect();
+        System.out.println(result);
+    }
 }
